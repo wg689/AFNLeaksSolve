42ba1cd3558d2a9e877f0daeec06309965071908
wg689 <596201463@qq.com>
2019-06-29 21:13:44 +0800

    修改好

--- 10-01/src/js/components/mobile_header.js
+++ 10-01/src/js/components/mobile_header.js
@@ -2,7 +2,7 @@ import React from 'react';
 export default class MobileHeader  extends React.Component {
 	render() {
 		return (
-			<div id='MobileHeader'>
+			<div id='mobileheader'>
 			  <header>
           <img src="./src/images/logo.png" alt="logo"/>
           <span>ReactNews</span>
