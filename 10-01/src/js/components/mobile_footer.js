a3e2d3417fe85f83d5753cabcb93af972105d226
wg689 <596201463@qq.com>
2019-06-29 21:56:18 +0800

    移动端的页脚开发

---
+++ 10-01/src/js/components/mobile_footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Row, Col} from 'antd';
+
+export default class PCFooter extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			current: 'top'
+		};
+	}
+	render() {
+		return (
+			<header>
+				<Row>
+					<Col span={2}></Col>
+					<Col span={20} class="footer">
+            &copy;&nbsp;2019react news. all rights Reserved.
+					</Col>
+					<Col span={2}></Col>
+				</Row>
+			</header>
+		);
+	};
+}
