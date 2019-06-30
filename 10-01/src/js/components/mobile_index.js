1a6a30b03119bb9b7f8ef769b6a56e5264599fd8
wg689 <596201463@qq.com>
2019-06-30 20:36:59 +0800

    图片大小不对

--- 10-01/src/js/components/mobile_index.js
+++ 10-01/src/js/components/mobile_index.js
@@ -3,6 +3,8 @@ import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer'
 import {Tabs} from 'antd'
 const TabPane = Tabs.TabPane;
+import MobileList from './mobile_list'
+
 
 export default class MobileIndex extends React.Component {
 	render() {
@@ -11,14 +13,15 @@ export default class MobileIndex extends React.Component {
 				<MobileHeader></MobileHeader>
 				<Tabs>
 					<TabPane tab="头条" key= "1">
+						<MobileList count={20} type="top"></MobileList>
 					</TabPane>
-					<TabPane tab="社会" key= "2">
+					<TabPane tab="社会" key= "shehui">
 					</TabPane>
-					<TabPane tab="国内" key= "3">
+					<TabPane tab="国内" key= "guonei">
 					</TabPane>
-					<TabPane tab="国际" key= "4">
+					<TabPane tab="国际" key= "guoji">
 					</TabPane>
-					<TabPane tab="娱乐" key= "5">
+					<TabPane tab="娱乐" key= "yule">
 					</TabPane>
 				</Tabs>
 				<MobileFooter></MobileFooter>
---
+++ 10-01/src/js/components/mobile_list.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Row,Col} from 'antd';
+import {Rooter,Route,Link,browserHistory} from 'react-router'
+export  default class MobileList extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      news:''
+    }
+  }
+  componentWillMount (){
+    var myFetchOptions = {
+      method: 'GET'
+    };
+    var url = "http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=" + this.props.type + "&count=" + this.props.count;
+    fetch(url,myFetchOptions)
+    .then(response=>response.json())
+    .then(json =>this.setState({news:json}));
+
+  };
+
+  render(){
+    const {news} = this.state;
+    const newsList = news.length
+    ?  news.map((newsItem,index) => (
+      <section key={index} className="m_article list-item special_section clearfix" >
+      <Link to ={`details/${newsItem.uniquekey}`}>
+        <div className="m_artical_img">
+          <img src={newsItem.thumbnail_pic_s} alt={newsItem.title}/>
+        </div>
+        <div className="m_artical_info">
+          <span>{newsItem.title}</span>
+        </div>
+        <div className="m_artical_desc clearfix">
+          <div className="m_artical_desc_l">
+            <span className="m_artical_channel">{newsItem.realtype}</span>
+            <span className="m_artical_time">{newsItem.date}</span>
+          </div>
+        </div>
+      </Link>
+      </section>
+    )): "没有加载到任何新闻";
+
+
+    return(
+      <div>
+        {newsList}
+      </div>
+    )
+  }
+}
